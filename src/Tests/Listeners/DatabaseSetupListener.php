83f3f3b9b4d84ec42b148b6c27fc51b149c32e3a
Takaaki Mizuno <takaaki.mizuno@gmail.com>
2017-04-13 16:43:47 +0800

    :green_heart: Revert Update

--- src/Tests/Listeners/DatabaseSetupListener.php
+++ src/Tests/Listeners/DatabaseSetupListener.php
@@ -5,14 +5,14 @@ class DatabaseSetupListener extends \PHPUnit_Framework_BaseTestListener
 {
     protected $suites = ['Application Test Suite'];
 
-    public function startTestSuite($suite)
+    public function startTestSuite(\PHPUnit_Framework_TestSuite $suite)
     {
         if (in_array($suite->getName(), $this->suites)) {
             exec('php artisan migrate');
         }
     }
 
-    public function endTestSuite($suite)
+    public function endTestSuite(\PHPUnit_Framework_TestSuite $suite)
     {
         if (in_array($suite->getName(), $this->suites)) {
             exec('php artisan migrate:rollback');
