82152aaeaae96f6a0790fa670363a6757ffb0edc
Takaaki Mizuno <takaaki.mizuno@gmail.com>
2017-04-13 16:35:28 +0800

    :green_heart: Database Listener Update

--- src/Tests/Listeners/DatabaseSetupListener.php
+++ src/Tests/Listeners/DatabaseSetupListener.php
@@ -5,14 +5,14 @@ class DatabaseSetupListener extends \PHPUnit_Framework_BaseTestListener
 {
     protected $suites = ['Application Test Suite'];
 
-    public function startTestSuite(\PHPUnit_Framework_TestSuite $suite)
+    public function startTestSuite($suite)
     {
         if (in_array($suite->getName(), $this->suites)) {
             exec('php artisan migrate');
         }
     }
 
-    public function endTestSuite(\PHPUnit_Framework_TestSuite $suite)
+    public function endTestSuite($suite)
     {
         if (in_array($suite->getName(), $this->suites)) {
             exec('php artisan migrate:rollback');
