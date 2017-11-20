28bddca65c31cff8b036c95d0bf880be443ee7be
Takaaki <takaaki.mizuno@gmail.com>
2017-11-20 14:46:42 +0900

    :+1: Client Error Exception

--- src/Exceptions/ClientErrorException.php
+++ src/Exceptions/ClientErrorException.php
@@ -5,8 +5,8 @@ use Exception;
 
 class ClientErrorException extends Exception
 {
-    public function __construct($error, $message, $userMessage = '')
+    public function __construct($error, $message = '', $userMessage = '')
     {
-        parent::__construct($error, $message, $userMessage = '');
+        parent::__construct($error, $message, $userMessage);
     }
 }
