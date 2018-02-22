bb0d0b85737301f460973a0f5785085397e411c5
Takaaki Mizuno <takaaki.mizuno@gmail.com>
2018-02-22 09:32:49 +0800

    :+1: Add DataHelper Facade

---
+++ src/Facades/DataHelper.php
@@ -0,0 +1,12 @@
+<?php
+namespace LaravelRocket\Foundation\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class DataHelper extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return 'LaravelRocket\Foundation\Helpers\DateTimeHelperInterface';
+    }
+}
