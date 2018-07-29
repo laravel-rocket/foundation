b35686d072d2e6b032bf8ed2ae0a0b40a2d9f4e1
Takaaki Mizuno <takaaki.mizuno@gmail.com>
2018-07-29 23:41:16 +0700

    :+1: Refactor String Helper

--- src/Helpers/Production/StringHelper.php
+++ src/Helpers/Production/StringHelper.php
@@ -1,57 +1,10 @@
 <?php
 namespace LaravelRocket\Foundation\Helpers\Production;
 
-use ICanBoogie\Inflector;
 use LaravelRocket\Foundation\Helpers\StringHelperInterface;
 
 class StringHelper implements StringHelperInterface
 {
-    // Ref: http://stackoverflow.com/questions/1993721/how-to-convert-camelcase-to-camel-case
-    public function camel2Snake($input)
-    {
-        preg_match_all('!([A-Z][A-Z0-9]*(?=$|[A-Z][a-z0-9])|[A-Za-z][a-z0-9]+)!', $input, $matches);
-        $ret = $matches[0];
-        foreach ($ret as &$match) {
-            $match = $match == strtoupper($match) ? strtolower($match) : lcfirst($match);
-        }
-
-        return implode('_', $ret);
-    }
-
-    public function camel2Spinal($input)
-    {
-        preg_match_all('!([A-Z][A-Z0-9]*(?=$|[A-Z][a-z0-9])|[A-Za-z][a-z0-9]+)!', $input, $matches);
-        $ret = $matches[0];
-        foreach ($ret as &$match) {
-            $match = $match == strtoupper($match) ? strtolower($match) : lcfirst($match);
-        }
-
-        return implode('-', $ret);
-    }
-
-    public function snake2Camel($input)
-    {
-        $string = preg_replace_callback('/(^|_)([a-z])/', function($match) {
-            return strtoupper($match[2]);
-        }, $input);
-
-        return lcfirst($string);
-    }
-
-    public function pluralize($singular)
-    {
-        $inflector = Inflector::get('en');
-
-        return $inflector->pluralize($singular);
-    }
-
-    public function singularize($plural)
-    {
-        $inflector = Inflector::get('en');
-
-        return $inflector->singularize($plural);
-    }
-
     public function randomString($length)
     {
         mt_rand();
@@ -64,18 +17,13 @@ class StringHelper implements StringHelperInterface
         return $result;
     }
 
-    // Ref: http://stackoverflow.com/questions/834303/startswith-and-endswith-functions-in-php
-    public function startsWith($haystack, $needle)
+    public function randomReadableString($length)
     {
-        return $needle === '' || strrpos($haystack, $needle, -strlen($haystack)) !== false;
-    }
-
-    public function endsWith($haystack, $needle)
-    {
-        return $needle === '' || (($temp = strlen($haystack) - strlen($needle)) >= 0 && strpos(
-            $haystack,
-            $needle,
-                    $temp
-        ) !== false);
+        mt_rand();
+        $characters = str_split('ABCDEFGHJKLMNPQRSTUVWXYZ23456789');
+        $result     = '';
+        for ($i = 0; $i < $length; ++$i) {
+            $result .= $characters[mt_rand(0, count($characters) - 1)];
+        }
     }
 }
--- src/Helpers/StringHelperInterface.php
+++ src/Helpers/StringHelperInterface.php
@@ -4,41 +4,6 @@ namespace LaravelRocket\Foundation\Helpers;
 interface StringHelperInterface
 {
     /**
-     * @param string $input
-     *
-     * @return string
-     */
-    public function camel2Snake($input);
-
-    /**
-     * @param string $input
-     *
-     * @return string
-     */
-    public function camel2Spinal($input);
-
-    /**
-     * @param string $input
-     *
-     * @return string
-     */
-    public function snake2Camel($input);
-
-    /**
-     * @param string $singular
-     *
-     * @return string mixed
-     */
-    public function pluralize($singular);
-
-    /**
-     * @param string $plural
-     *
-     * @return string
-     */
-    public function singularize($plural);
-
-    /**
      * @param int $length
      *
      * @return string
@@ -46,18 +11,9 @@ interface StringHelperInterface
     public function randomString($length);
 
     /**
-     * @param string $haystack
-     * @param string $needle
+     * @param int $length
      *
-     * @return bool
+     * @return string
      */
-    public function startsWith($haystack, $needle);
-
-    /**
-     * @param string $haystack
-     * @param string $needle
-     *
-     * @return bool
-     */
-    public function endsWith($haystack, $needle);
+    public function randomReadableString($length);
 }
