0542b824bf84dba24bb98608c543371f2881ed71
Takaaki Mizuno <takaaki.mizuno@gmail.com>
2018-12-08 17:18:25 +0800

    :recycle: Refactoring

--- src/Exports/AdminModelExport.php
+++ src/Exports/AdminModelExport.php
@@ -22,52 +22,15 @@ class AdminModelExport implements FromQuery
         $this->exportService = app()->make(\LaravelRocket\Foundation\Services\ExportServiceInterface::class);
     }
 
-    /**
-     * @param string $modelName
-     *
-     * @return \LaravelRocket\Foundation\Models\Base|null
-     */
-    protected function getModelInstance(string $modelName)
-    {
-        $modelClass = '\\App\\Models\\'.$modelName;
-        if (!class_exists($modelClass)) {
-            return;
-        }
-
-        /** @var \LaravelRocket\Foundation\Models\Base $modelInstance */
-        $modelInstance = new $modelClass();
-
-        return $modelInstance;
-    }
-
-    /**
-     * @param string $modelName
-     *
-     * @return \LaravelRocket\Foundation\Repositories\Eloquent\SingleKeyModelRepository|null
-     */
-    protected function getRepository(string $modelName)
-    {
-        $repositoryInterfaceClass = 'App\\Repositories\\'.$modelName.'RepositoryInterface';
-
-        if (!interface_exists($repositoryInterfaceClass)) {
-            return;
-        }
-
-        /** @var \LaravelRocket\Foundation\Repositories\Eloquent\SingleKeyModelRepository $repository */
-        $repository = app()->make($repositoryInterfaceClass);
-
-        return $repository;
-    }
-
     public function query()
     {
-        $modelInstance = $this->getModelInstance($this->modelName);
+        $modelInstance = $this->exportService->getModel($this->modelName);
         if (empty($modelInstance)) {
-            return;
+            return null;
         }
-        $repository = $this->getRepository($this->modelName);
+        $repository = $this->exportService->getRepository($this->modelName);
         if (empty($repository)) {
-            return;
+            return null;
         }
 
         return $repository->allByFilterQuery([], 'id', 'asc');
